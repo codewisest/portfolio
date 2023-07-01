4ebe22a9e0121c109c1ac4e0a52a6ee4d5280f0f
Chijioke <wisestcodes@gmail.com>
2023-07-01 15:17:30 -0400

    dynamic caching

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = "site-static-v1";
+const dynamicCache = "site-dynamic-v1";
 const assets = [
   "/",
   "/index.html",
@@ -40,7 +41,15 @@ self.addEventListener("fetch", (evt) => {
   // console.log("fetch event", evt);
   evt.respondWith(
     caches.match(evt.request).then((cacheResponse) => {
-      return cacheResponse || fetch(evt.request);
+      return (
+        cacheResponse ||
+        fetch(evt.request).then((fetchRes) => {
+          return caches.open(dynamicCache).then((cache) => {
+            cache.put(evt.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
     })
   );
 });
